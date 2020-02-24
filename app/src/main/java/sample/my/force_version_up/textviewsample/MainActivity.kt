552fa440ac85036061a7c17b9e0776024c58fced
shinya-takano <dev.shinya.takano@gmail.com>
2020-02-24 13:41:06 +0900

    Initial commit

---
+++ app/src/main/java/sample/my/force_version_up/textviewsample/MainActivity.kt
@@ -0,0 +1,15 @@
+package sample.my.force_version_up.textviewsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "savedInstanceState=$savedInstanceState")
+    }
+}
